236227bfb1472c4445d2d87456cd6e467b4ce1f6
Jordi Prats <jordi.prats@gmail.com>
2020-09-04 13:46:09 +0200

    forcentp

--- spec/packer/puppet/site.pp
+++ spec/packer/puppet/site.pp
@@ -1,5 +1,6 @@
 class { 'ntp':
-  servers => [
+  force_ntpd => true,
+  servers    => [
                 '1.ie.pool.ntp.org',
                 '0.europe.pool.ntp.org',
                 '3.europe.pool.ntp.org'
